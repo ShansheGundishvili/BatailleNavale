b141db86a4b5ea6d2e571e48b05dcfa7291b9f30
GUNDISHVILI Shanshe <Shanshe.GUNDISHVILI@cpnv.ch>
2020-03-05 15:45:55 +0100

    adding new page of gameplay

---
+++ BN_Gameplay.c
@@ -0,0 +1,10 @@
+//
+// Created by shanshe.gundishvili on 05.03.2020.
+//
+int main() {
+
+
+
+    return 0;
+}
+
--- main.c
+++ main.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
+void newNameF(){
+    char newNameNN;
+    printf("1. the new name has to be unique than others that have been already created\n"
+           "2. this name will be saved and you can use it again\n"
+           "3. scores will be saved if its bigger than top 5 with the name you play with");
+    printf("\n\nEnter your new name");
+    scanf("%s", &newNameNN);
 
+
+
+}
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int choiceOneM;
+    char nameM;
+    printf("Menu\n\n");
+    printf("\n1. Play");
+    printf("\n2. Get new name");
+    printf("\n3. Scoreboard");
+    printf("\n4. Rules & Help");
+    printf("\n5. Credit & Code");
+    printf("\n6. quit the game");
+    printf("\nYour choice : ");
+    scanf("%d", &choiceOneM);
+    switch (choiceOneM)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            return 0;
+
+
+
+    }
+return 0;
 }
