cf79a1f39dd40a959531161f0563ee29ed9b8389
GUNDISHVILI Shanshe <Shanshe.GUNDISHVILI@cpnv.ch>
2020-03-12 16:47:04 +0100

    changed file and added rules as a function but not finished

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # DemoGitMA20
+
+# this game is udeveloped for this time just 0.1 version
+# but this game can play a succsesful game
+# can count scores
+# and can count misses
--- main.c
+++ main.c
@@ -1,4 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int gameplay() {
+    int j;
+    int i;
+    int shipNB=17;
+    int pts=100;
+    int choiceX;
+    int choiceY;
+    int mapN1[10][10]={1, 1, 1, 1, 1, 0, 1, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 0, 0, 1, 1, 1, 0, 0, 0,
+                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+                       0, 0, 1, 0, 0, 1, 0, 0, 0, 0,
+                       0, 0, 1, 0, 0, 1, 0, 0, 0, 0};
+    system("cls");
+    do {
+        printf("\n\nyour points: %d", pts);
+        for (i = 0; i < 10; ++i) {
+            printf("\n");
+            for (j = 0; j < 10; ++j) {
+                if(mapN1[i][j]<2){
+                    printf("%2c%d", 65+i, j+1);
+                }else if(mapN1[i][j]==2){
+                    printf("\033[1;35m");
+                    printf("%2c%d", 65+i, j+1);
+                    printf("\033[0m");
+                }
+                else if(mapN1[i][j]==3){
+                    printf("\033[1;31m");
+                    printf("%2c%d", 65+i, j+1);
+                    printf("\033[0m");
+                }
+            }
+
+        }
+        printf("\n\nships left: %d\n",shipNB);
+        do {
+            printf("input X:");
+            scanf("%d", &choiceX);
+        }while((choiceX>10) || (choiceX<1));
+        do {
+            printf("input Y:");
+            scanf("%d", &choiceY);
+        }while((choiceY>10) || (choiceY<1));
+        system("cls");
+        choiceX--;
+        choiceY--;
+
+        if (mapN1[choiceY][choiceX] == 1) {
+            printf("you hit something!!!");
+            mapN1[choiceY][choiceX] = 3;
+            shipNB--;
+            //1 = there is a ship   //2 = shot and miss  //3 = shot ship already
+        } else if (mapN1[choiceY][choiceX] == 0) {
+            printf("you miss them");
+            mapN1[choiceY][choiceX] = 2;
+            pts -= 1;
+        } else if (mapN1[choiceY][choiceX] == 2) {
+            printf("already shot there, there was nothing");
+            pts -= 2;
+        } else if (mapN1[choiceY][choiceX] == 3) {
+            printf("already shot there, there was a battleship");
+            pts -= 1;
+        }
+    }while (shipNB>0);
+
+    return 0;
+
+}
 void newNameF(){
     char newNameNN;
     printf("1. the new name has to be unique than others that have been already created\n"
@@ -9,23 +84,28 @@ void newNameF(){
 
 
 
+    void rules(){
+        printf("Rules\n\n");
+        printf("hello lost player!");
+        printf("\nto escape from sea you have to defeat K the 11th");
+        printf("The SS level MAP");
+        printf("You have to find the ships this Map is hiding and destroy them with your artillery");
+        printf("But be careful chance to escape goes down if you miss the shot by one ");
+        printf("");
+        printf("");
+        printf("");
+
+
+
+    }
+
+
+
 }
 int main() {
+    int points;
     int choiceOneM;
-    int userNumberX=0;
-    char userLetterY[1];
-    int userNumberY=0;
-    int mapN1[10][10]={1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
-                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                       0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    char nameM;
+
     printf("Menu\n\n");
     printf("\n1. Play");
     printf("\n2. Get new name");
@@ -38,47 +118,8 @@ int main() {
     switch (choiceOneM)
     {
         case 1:
-
-            printf("enter X coordinates to attack (max 10): ");
-            scanf("%d", &userNumberX);
-            printf("enter X coordinates to attack (A to J(UPPER LETTERS)): ");
-            scanf("%s", &userLetterY);
-            do {
-                if (userLetterY == "A") {
-                    userNumberY = 1;
-                } else if (userLetterY == "B") {
-                    userNumberY = 2;
-                } else if (userLetterY == "C") {
-                    userNumberY = 3;
-                } else if (userLetterY == "D") {
-                    userNumberY = 4;
-                } else if (userLetterY == "E") {
-                    userNumberY = 5;
-                } else if (userLetterY == "F") {
-                    userNumberY = 6;
-                } else if (userLetterY == "G") {
-                    userNumberY = 7;
-                } else if (userLetterY == "H") {
-                    userNumberY = 8;
-                } else if (userLetterY == "I") {
-                    userNumberY = 9;
-                } else if (userLetterY == "J") {
-                    userNumberY = 10;
-                } else {
-                    printf("invalid answer");
-                    printf("enter X coordinates to attack (A to J(UPPER LETTERS))");
-                }
-            }while (userNumberX);
-            if (mapN1[userNumberX][userNumberY]==1)
-            {
-                mapN1[userNumberX][userNumberY]=3;
-                printf("you hit them!!!");
-            }
-            else{
-                mapN1[userNumberX][userNumberY]=2;
-                printf("you miss them, aim better");
-            }
-
+            system("cls");
+            points=gameplay();
             break;
         case 2:
             break;
@@ -94,5 +135,5 @@ int main() {
 
 
     }
-return 0;
+    return 0;
 }
